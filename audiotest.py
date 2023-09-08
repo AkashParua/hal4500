b003c18eec7416ddd3b1c25f9775b7b214b63b1d
akash <akashparua999@gmail.com>
2023-09-09 02:09:57 +0530

    added .env to gitignore

--- audiotest.py
+++ audiotest.py
@@ -3,13 +3,23 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-data = None
+
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.setblocking(False)
+
 soc.connect((socket.gethostname(), int(os.getenv("HEARING_PORT"))))
 while True:
-    data = soc.recv(1024).decode('utf-8')
+    data = None
+    try :
+        data = soc.recv(1024).decode('utf-8')
+    except BlockingIOError:
+        pass
+
     if data:
-        print(data)
         if data == "exit":
             break
+    
+        print(data)
+
+
 soc.close()
